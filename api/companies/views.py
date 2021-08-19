ee0ea9816ee144ed66923d3730cd2003984e2f3f
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-08-19 12:56:18 +0200

    first commit

---
+++ api/companies/views.py
@@ -0,0 +1,10 @@
+from companies.models import Company
+from rest_framework.pagination import PageNumberPagination
+from companies.serializers import CompanySerializer
+from rest_framework.viewsets import ModelViewSet
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by('-last_update')
+    pagination_class = PageNumberPagination
